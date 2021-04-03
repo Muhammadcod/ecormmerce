352d339d3c0b9d21071c97d30e657d694f435eb0
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-04-03 10:27:50 +0100

    feat: User slice

--- clients/src/features/user/UsersSlice.js
+++ clients/src/features/user/UsersSlice.js
@@ -4,12 +4,18 @@ import {
   createEntityAdapter,
 } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { fetchProducts } from '../products/productsSlice';
 
-export const fetchUsers = createAsyncThunk('Users/fetchUsers', async () => {
-  const response = await axios.get('http://localhost:3002/users');
-  return response.data;
-});
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (userDetails) => {
+    console.log('===', userDetails);
+    const response = await axios.post(
+      'http://localhost:3002/login',
+      userDetails
+    );
+    return response.data;
+  }
+);
 
 const usersAdapter = createEntityAdapter({
   sortComparer: (a, b) => b.date.localeCompare(a.date),
@@ -23,12 +29,10 @@ const initialState = usersAdapter.getInitialState({
 export const signUpUser = createAsyncThunk(
   'user/signUpUser',
   async (userProfile) => {
-    console.log('===', userProfile);
-    const response = await axios.post('http://localhost:3002/signup', {
-      headers: {
-        Authorization: `token ${access_token}`,
-      },
-    });
+    const response = await axios.post(
+      'http://localhost:3002/signup',
+      userProfile
+    );
     return response.data;
   }
 );
@@ -37,34 +41,28 @@ const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-  [fetchUsers.pending]: (state, action) => {
-    state.status = 'loading';
-  },
-  [fetchUsers.fulfilled]: (state, action) => {
-    state.status = 'succeeded';
-    // Add any fetched products to the array
-    state.products = state.products.concat(action.payload);
-  },
-  [fetchUsers.rejected]: (state, action) => {
-    state.status = 'failed';
-    state.error = action.error.message;
-  },
-  /*extraReducers: {
+  extraReducers: {
+    [loginUser.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      state.status = 'succeeded';
+    },
+    [loginUser.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    },
     [signUpUser.pending]: (state, action) => {
       state.status = 'loading';
     },
     [signUpUser.fulfilled]: (state, action) => {
       state.status = 'succeeded';
-      // Add any fetched products to the array
-      state.email = action.payload.user.email;
-      state.username = action.payload.user.name;
-      return state;
     },
     [signUpUser.rejected]: (state, action) => {
       state.status = 'failed';
       state.error = action.error.message;
     },
-  },*/
+  },
 });
 
 export const { addUser } = usersSlice.actions;
