fc9a120a2f7c288688833ca01ceff44317c77f42
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-04-03 10:28:37 +0100

    feat: Login route

--- clients/src/App.js
+++ clients/src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import ProductContainer from './features/products/ProductContainer';
 import SignUp from './features/user/SignUp';
 import AddNewProduct from './features/products/AddNewProduct';
+import Login from './features/user/Login';
 
 // import AddNewProduct from './features/products/AddNewProduct';
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={ProductContainer} />
         <Route path="/signup" exact component={SignUp} />
+        <Route path="/login" exact component={Login} />
         <Route path="/create" exact component={AddNewProduct} />
       </Switch>
     </div>
